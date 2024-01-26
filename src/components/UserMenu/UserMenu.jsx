17b05261b08240dfb53691cf171c649329338234
Ваня <7328652@gmail.com>
2024-01-26 17:07:44 +0200

    changed header, added UserMenu.

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,7 +1,38 @@
-import {} from './UserMenu.styled';
+import { useSelector } from 'react-redux';
+import { selectAuthAuthenticated } from 'redux/auth/auth.selectors';
+
+import { NavLinkSignin, UserSvg, ArrowDownSvg } from './UserMenu.styled';
+import icons from '../../img/icons.svg';
+import avatar from '../../img/avatar.png';
 
 const UserMenu = () => {
-  return <></>;
+  const authenticated = useSelector(selectAuthAuthenticated);
+
+  const UserName = 'David';
+
+  return (
+    <>
+      {authenticated ? (
+        <NavLinkSignin to="signin">
+          {UserName}
+          <img src={avatar} alt={UserName} width="28" height="28" />
+          <UserSvg width="28" height="28">
+            <use href={icons + '#icon-user'}></use>
+          </UserSvg>
+          <ArrowDownSvg width="16" height="16">
+            <use href={icons + '#icon-arrow-down'}></use>
+          </ArrowDownSvg>
+        </NavLinkSignin>
+      ) : (
+        <NavLinkSignin to="signin">
+          <span>Sign in</span>
+          <UserSvg width="28" height="28">
+            <use href={icons + '#icon-user'}></use>
+          </UserSvg>
+        </NavLinkSignin>
+      )}
+    </>
+  );
 };
 
 export default UserMenu;
