a385623c65cf8549f5f6934a968aae48b5c60810
Ваня <7328652@gmail.com>
2024-01-30 03:11:09 +0200

    commented LoginSection in LoginPage

--- src/page/LoginPage.jsx
+++ src/page/LoginPage.jsx
@@ -1,6 +1,7 @@
 import LogInForm from 'components/LogInForm/LogIn';
 import { Section } from 'components/Section/Section';
-import { LoginSection, LoginContainer } from './LoginPage.styled';
+// import { LoginSection, LoginContainer } from './LoginPage.styled';
+import { LoginContainer } from './LoginPage.styled';
 
 const LogInPage = () => {
   return (
