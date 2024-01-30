7b9e365243f7bd563e0417b9a136330be80c9b38
Ваня <7328652@gmail.com>
2024-01-30 02:54:39 +0200

    upd setting form and styles

--- src/components/Setting/Setting.jsx
+++ src/components/Setting/Setting.jsx
@@ -3,21 +3,27 @@ import * as Yup from 'yup';
 
 import {
   Backdrop,
-  ContaineWrapper,
   Modal,
   CaptionBlock,
   PhotoBlock,
   FormStyle,
-  GenderBlock,
-  UserName,
-  UserEmail,
-  PasswordBlock,
+  GenderPart,
+  RadioLabel,
+  InputHidden,
+  UserInfoBox,
+  PasswordBox,
+  BtnSettingSave,
+  PasswordInputContainer,
 } from './Setting.styled';
 
 import icons from '../../img/icons.svg';
 import settingAvatar from '../../img/setting-avatar.png';
+import { useState } from 'react';
 
-const Setting = () => {
+const Setting = ({ closeModal }) => {
+  const [isChecked, setIsChecked] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+
   const validationSchema = Yup.object({
     name: Yup.string().required('Name is required'),
     email: Yup.string()
@@ -37,91 +43,165 @@ const Setting = () => {
     validationSchema: validationSchema,
   });
 
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
   return (
     <>
       <Backdrop>
-        <ContaineWrapper>
-          <Modal>
-            <CaptionBlock>
-              <h2>Setting</h2>
+        <Modal>
+          <CaptionBlock>
+            <h2>Setting</h2>
+            <button type="button" onClick={closeModal}>
+              <svg width="24" height="24">
+                <use href={icons + '#icon-close-setting'}></use>
+              </svg>
+            </button>
+          </CaptionBlock>
+          <PhotoBlock>
+            <h3>Your photo</h3>
+            <div>
+              <img
+                src={settingAvatar}
+                alt="avatarName"
+                width={80}
+                height={80}
+              />
               <button type="button">
-                <svg width="24" height="24">
-                  <use href={icons + '#icon-close-setting'}></use>
+                <svg width="16" height="16">
+                  <use href={icons + '#icon-upload'}></use>
                 </svg>
+                Upload a photo
               </button>
-            </CaptionBlock>
+            </div>
+          </PhotoBlock>
 
-            <PhotoBlock>
-              <h3>Your photo</h3>
+          <FormStyle onSubmit={formik.handleSubmit}>
+            <GenderPart>
+              <h3>Your gender identity</h3>
               <div>
-                <img
-                  src={settingAvatar}
-                  alt="avatarName"
-                  width={80}
-                  height={80}
-                />
-                <button type="button">
-                  <svg width="16" height="16">
-                    <use href={icons + '#icon-upload'}></use>
-                  </svg>
-                  Upload a photo
-                </button>
+                <RadioLabel>
+                  <InputHidden
+                    type="radio"
+                    id="woman"
+                    name="gender"
+                    value="woman"
+                    checked={!isChecked}
+                    onChange={() => setIsChecked(!isChecked)}
+                  />
+                  <label htmlFor="woman">
+                    <svg width="14" height="14">
+                      <use href={icons + '#icon-checkbox'}></use>
+                    </svg>
+                    Woman
+                  </label>
+                </RadioLabel>
+
+                <RadioLabel>
+                  <InputHidden
+                    type="radio"
+                    id="man"
+                    name="gender"
+                    value="man"
+                    checked={isChecked}
+                    onChange={() => setIsChecked(!isChecked)}
+                  />
+                  <label htmlFor="man">
+                    <svg width="14" height="14">
+                      <use href={icons + '#icon-checkbox'}></use>
+                    </svg>
+                    Man
+                  </label>
+                </RadioLabel>
               </div>
-            </PhotoBlock>
+            </GenderPart>
 
-            <FormStyle onSubmit={formik.handleSubmit}>
-              <GenderBlock>
-                <h3>Your gender identity</h3>
-                <div>
-                  <input type="radio" id="woman" name="gender" value="woman" />
-                  <label htmlFor="woman">Woman</label>
+            <UserInfoBox>
+              <h3>Your name</h3>
+              <input type="text" placeholder="Name" name="user-name" />
+            </UserInfoBox>
+            <UserInfoBox>
+              <h3>E-mail</h3>
+              <input type="text" placeholder="E-mail" name="e-mail" />
+            </UserInfoBox>
 
-                  <input type="radio" id="man" name="gender" value="man" />
-                  <label htmlFor="man">Man</label>
-                </div>
-              </GenderBlock>
-              <UserName>
-                <h3>Your name</h3>
-                <input type="text" placeholder="Name" name="user-name" />
-              </UserName>
-              <UserEmail>
-                <h3>E-mail</h3>
-                <input type="text" placeholder="E-mail" name="e-mail" />
-              </UserEmail>
-              <PasswordBlock>
-                <h3>Password</h3>
-                <div>
-                  <h4>Outdated password:</h4>
+            <PasswordBox>
+              <h3>Password</h3>
+              <div>
+                <h4>Outdated password:</h4>
+                <PasswordInputContainer>
+                  <button type="button" onClick={togglePasswordVisibility}>
+                    <svg width="16" height="16">
+                      <use
+                        href={
+                          icons +
+                          (showPassword
+                            ? '#icon-opend-eye'
+                            : '#icon-closed-eye')
+                        }
+                      ></use>
+                    </svg>
+                  </button>
                   <input
-                    type="password"
+                    type={showPassword ? 'text' : 'password'}
                     placeholder="Password"
                     name="password"
                   />
-                </div>
-                <div>
-                  <h4>New Password:</h4>
+                </PasswordInputContainer>
+              </div>
+              <div>
+                <h4>New Password:</h4>
+                <PasswordInputContainer>
+                  <button type="button" onClick={togglePasswordVisibility}>
+                    <svg width="16" height="16">
+                      <use
+                        href={
+                          icons +
+                          (showPassword
+                            ? '#icon-opend-eye'
+                            : '#icon-closed-eye')
+                        }
+                      ></use>
+                    </svg>
+                  </button>
                   <input
-                    type="password"
+                    type={showPassword ? 'text' : 'password'}
                     placeholder="Password"
                     name="password"
                   />
-                </div>
-                <div>
-                  <h4>Repeat new password:</h4>
+                </PasswordInputContainer>
+              </div>
+              <div>
+                <h4>Repeat new password:</h4>
+                <PasswordInputContainer>
+                  <button type="button" onClick={togglePasswordVisibility}>
+                    <svg width="16" height="16">
+                      <use
+                        href={
+                          icons +
+                          (showPassword
+                            ? '#icon-opend-eye'
+                            : '#icon-closed-eye')
+                        }
+                      ></use>
+                    </svg>
+                  </button>
                   <input
-                    type="password"
+                    type={showPassword ? 'text' : 'password'}
                     placeholder="Password"
                     name="password"
                   />
-                </div>
-              </PasswordBlock>
-              <button type="submit">Save</button>
-            </FormStyle>
-          </Modal>
-        </ContaineWrapper>
+                </PasswordInputContainer>
+              </div>
+            </PasswordBox>
+            <BtnSettingSave type="submit">Save</BtnSettingSave>
+          </FormStyle>
+        </Modal>
       </Backdrop>
     </>
   );
 };
 
 export default Setting;
+
+// lplplp@lp.lp
--- src/components/Setting/Setting.styled.js
+++ src/components/Setting/Setting.styled.js
@@ -6,7 +6,7 @@ export const Backdrop = styled.div`
   left: 0;
   background: rgba(0, 0, 0, 0.8);
   width: 100%;
-  min-height: 100%;
+  height: 100%;
   opacity: 1;
   visibility: visible;
   transition: opacity 250ms cubic-bezier(0.4, 0, 0.2, 1),
@@ -21,18 +21,13 @@ export const Backdrop = styled.div`
     display: none;
   }
 `;
-export const ContaineWrapper = styled.div``;
-
 export const Modal = styled.div`
-  display: flex;
-  gap: 24px;
-
-  flex-direction: column;
   position: absolute;
   top: 40px;
   left: 50%;
   width: 280px;
   padding: 32px 12px;
+  border-radius: 10px;
 
   transform: translate(-50%, 0) scale(1);
   background-color: var(--primery-color-white);
@@ -51,6 +46,7 @@ export const CaptionBlock = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  margin-bottom: 24px;
   h2 {
     color: var(--primery-color-black);
     font-size: 26px;
@@ -63,13 +59,13 @@ export const CaptionBlock = styled.div`
     background: none;
     border: none;
     &:is(:hover, :focus) svg {
-      stroke: var(--primery-color-blue);
+      stroke: var(--primery-color-black);
     }
   }
 
   svg {
     fill: none;
-    stroke: var(--primery-color-black);
+    stroke: var(--primery-color-blue);
   }
 `;
 
@@ -78,6 +74,7 @@ export const PhotoBlock = styled.div`
     display: flex;
     align-items: center;
     gap: 8px;
+    margin-bottom: 24px;
   }
 
   button {
@@ -105,17 +102,129 @@ export const FormStyle = styled.form`
   gap: 24px;
 `;
 
-export const GenderBlock = styled.div`
-  div {
+export const GenderPart = styled.div`
+  & > div {
     display: flex;
+    gap: 24px;
   }
 `;
 
-export const UserName = styled.div``;
-export const UserEmail = styled.div``;
+export const RadioLabel = styled.div`
+  label {
+    display: flex;
+    align-items: center;
+    gap: 8px;
 
-export const PasswordBlock = styled.div`
+    color: var(--primery-color-black);
+    font-size: 16px;
+    line-height: 1.25;
+  }
+`;
+
+export const InputHidden = styled.input`
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  margin: -1px;
+  border: 0;
+  padding: 0;
+  white-space: nowrap;
+  clip-path: inset(100%);
+  clip: rect(0 0 0 0);
+  overflow: hidden;
+  & + label svg {
+    fill: none;
+  }
+  &:checked + label svg {
+    fill: var(--primery-color-blue);
+  }
+`;
+
+export const UserInfoBox = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  input {
+    padding: 12px 10px;
+
+    border-radius: 6px;
+    border: 1px solid #d7e3ff;
+    line-height: 1.25;
+    font-size: 16px;
+    color: var(--primery-color-blue);
+
+    &::placeholder {
+      color: var(--primery-color-blue);
+    }
+  }
+`;
+
+export const PasswordBox = styled.div`
   display: flex;
   flex-direction: column;
   gap: 12px;
+  & > div {
+    display: flex;
+    flex-direction: column;
+  }
+  h3 {
+    margin-bottom: 0;
+  }
+  h4 {
+    color: var(--primery-color-black);
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 1.25;
+    margin-bottom: 8px;
+  }
+  input {
+    padding: 12px 10px;
+    border-radius: 6px;
+    border: 1px solid var(--btn-color-light-blue);
+    line-height: 1.25;
+    font-size: 16px;
+    color: var(--secondary-color-blue);
+
+    &::placeholder {
+      color: var(--secondary-color-blue);
+    }
+  }
+  button {
+    position: absolute;
+    width: 20px;
+    height: 20px;
+    right: 10px;
+    top: 12px;
+    background: var(--primery-color-white);
+    text-transform: uppercase;
+    border: none;
+  }
+  svg {
+    fill: none;
+    stroke: var(--primery-color-black);
+    &:is(:hover, :focus, :active) {
+      stroke: var(--primery-color-blue);
+    }
+  }
+`;
+
+export const PasswordInputContainer = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const BtnSettingSave = styled.button`
+  display: flex;
+  padding: 8px 30px;
+  justify-content: center;
+
+  border-radius: 10px;
+  background: var(--primery-color-blue);
+  box-shadow: 0px 4px 8px 0px rgba(64, 123, 255, 0.34);
+
+  color: var(--primery-color-white);
+  font-weight: 500;
+  line-height: 1.25;
+  border: none;
 `;
