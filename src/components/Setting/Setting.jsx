7daaaa6d9fa82fbe9c5e4fc968229d3bb4e421ea
Ваня <7328652@gmail.com>
2024-01-30 21:20:11 +0200

    upd README

--- src/components/Setting/Setting.jsx
+++ src/components/Setting/Setting.jsx
@@ -3,13 +3,16 @@ import * as Yup from 'yup';
 
 import {
   Backdrop,
+  WrapperModal,
   Modal,
   CaptionBlock,
   PhotoBlock,
   FormStyle,
+  FormWrapper,
   GenderPart,
   RadioLabel,
   InputHidden,
+  ContainerUserInfo,
   UserInfoBox,
   PasswordBox,
   BtnSettingSave,
@@ -18,7 +21,7 @@ import {
 
 import icons from '../../img/icons.svg';
 import settingAvatar from '../../img/setting-avatar.png';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const Setting = ({ closeModal }) => {
   const [isChecked, setIsChecked] = useState(false);
@@ -26,9 +29,7 @@ const Setting = ({ closeModal }) => {
 
   const validationSchema = Yup.object({
     name: Yup.string().required('Name is required'),
-    email: Yup.string()
-      .required('Email is required')
-      .email('Invalid email address'),
+    email: Yup.string().required('Email is required').email('Invalid email address'),
     password: Yup.string().required('Password is requird'),
   });
 
@@ -46,162 +47,157 @@ const Setting = ({ closeModal }) => {
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
+
+  useEffect(() => {
+    const onEsc = e => e.key === 'Escape' && closeModal();
+
+    window.addEventListener('keydown', onEsc);
+    return () => window.removeEventListener('keydown', onEsc);
+  }, [closeModal]);
+
   return (
     <>
-      <Backdrop>
-        <Modal>
-          <CaptionBlock>
-            <h2>Setting</h2>
-            <button type="button" onClick={closeModal}>
-              <svg width="24" height="24">
-                <use href={icons + '#icon-close-setting'}></use>
-              </svg>
-            </button>
-          </CaptionBlock>
-          <PhotoBlock>
-            <h3>Your photo</h3>
-            <div>
-              <img
-                src={settingAvatar}
-                alt="avatarName"
-                width={80}
-                height={80}
-              />
-              <button type="button">
-                <svg width="16" height="16">
-                  <use href={icons + '#icon-upload'}></use>
+      <Backdrop className="backdrop">
+        <WrapperModal>
+          <Modal>
+            <CaptionBlock>
+              <h2>Setting</h2>
+              <button type="button" onClick={closeModal}>
+                <svg width="24" height="24">
+                  <use href={icons + '#icon-close-setting'}></use>
                 </svg>
-                Upload a photo
               </button>
-            </div>
-          </PhotoBlock>
-
-          <FormStyle onSubmit={formik.handleSubmit}>
-            <GenderPart>
-              <h3>Your gender identity</h3>
+            </CaptionBlock>
+            <PhotoBlock>
+              <h3>Your photo</h3>
               <div>
-                <RadioLabel>
-                  <InputHidden
-                    type="radio"
-                    id="woman"
-                    name="gender"
-                    value="woman"
-                    checked={!isChecked}
-                    onChange={() => setIsChecked(!isChecked)}
-                  />
-                  <label htmlFor="woman">
-                    <svg width="14" height="14">
-                      <use href={icons + '#icon-checkbox'}></use>
-                    </svg>
-                    Woman
-                  </label>
-                </RadioLabel>
+                <img src={settingAvatar} alt="avatarName" width={80} height={80} />
+                <button type="button">
+                  <svg width="16" height="16">
+                    <use href={icons + '#icon-upload'}></use>
+                  </svg>
+                  Upload a photo
+                </button>
+              </div>
+            </PhotoBlock>
 
-                <RadioLabel>
-                  <InputHidden
-                    type="radio"
-                    id="man"
-                    name="gender"
-                    value="man"
-                    checked={isChecked}
-                    onChange={() => setIsChecked(!isChecked)}
-                  />
-                  <label htmlFor="man">
-                    <svg width="14" height="14">
-                      <use href={icons + '#icon-checkbox'}></use>
-                    </svg>
-                    Man
-                  </label>
-                </RadioLabel>
-              </div>
-            </GenderPart>
+            <FormStyle onSubmit={formik.handleSubmit}>
+              <FormWrapper>
+                <div>
+                  <GenderPart>
+                    <h3>Your gender identity</h3>
+                    <div>
+                      <RadioLabel>
+                        <InputHidden
+                          type="radio"
+                          id="woman"
+                          name="gender"
+                          value="woman"
+                          checked={!isChecked}
+                          onChange={() => setIsChecked(!isChecked)}
+                        />
+                        <label htmlFor="woman">
+                          <svg width="14" height="14">
+                            <use href={icons + '#icon-checkbox'}></use>
+                          </svg>
+                          Woman
+                        </label>
+                      </RadioLabel>
 
-            <UserInfoBox>
-              <h3>Your name</h3>
-              <input type="text" placeholder="Name" name="user-name" />
-            </UserInfoBox>
-            <UserInfoBox>
-              <h3>E-mail</h3>
-              <input type="text" placeholder="E-mail" name="e-mail" />
-            </UserInfoBox>
+                      <RadioLabel>
+                        <InputHidden
+                          type="radio"
+                          id="man"
+                          name="gender"
+                          value="man"
+                          checked={isChecked}
+                          onChange={() => setIsChecked(!isChecked)}
+                        />
+                        <label htmlFor="man">
+                          <svg width="14" height="14">
+                            <use href={icons + '#icon-checkbox'}></use>
+                          </svg>
+                          Man
+                        </label>
+                      </RadioLabel>
+                    </div>
+                  </GenderPart>
+                  <ContainerUserInfo>
+                    <UserInfoBox>
+                      <h3>Your name</h3>
+                      <input type="text" placeholder="Name" name="user-name" />
+                    </UserInfoBox>
+                    <UserInfoBox>
+                      <h3>E-mail</h3>
+                      <input type="text" placeholder="E-mail" name="e-mail" />
+                    </UserInfoBox>
+                  </ContainerUserInfo>
+                </div>
 
-            <PasswordBox>
-              <h3>Password</h3>
-              <div>
-                <h4>Outdated password:</h4>
-                <PasswordInputContainer>
-                  <button type="button" onClick={togglePasswordVisibility}>
-                    <svg width="16" height="16">
-                      <use
-                        href={
-                          icons +
-                          (showPassword
-                            ? '#icon-opend-eye'
-                            : '#icon-closed-eye')
-                        }
-                      ></use>
-                    </svg>
-                  </button>
-                  <input
-                    type={showPassword ? 'text' : 'password'}
-                    placeholder="Password"
-                    name="password"
-                  />
-                </PasswordInputContainer>
-              </div>
-              <div>
-                <h4>New Password:</h4>
-                <PasswordInputContainer>
-                  <button type="button" onClick={togglePasswordVisibility}>
-                    <svg width="16" height="16">
-                      <use
-                        href={
-                          icons +
-                          (showPassword
-                            ? '#icon-opend-eye'
-                            : '#icon-closed-eye')
-                        }
-                      ></use>
-                    </svg>
-                  </button>
-                  <input
-                    type={showPassword ? 'text' : 'password'}
-                    placeholder="Password"
-                    name="password"
-                  />
-                </PasswordInputContainer>
-              </div>
-              <div>
-                <h4>Repeat new password:</h4>
-                <PasswordInputContainer>
-                  <button type="button" onClick={togglePasswordVisibility}>
-                    <svg width="16" height="16">
-                      <use
-                        href={
-                          icons +
-                          (showPassword
-                            ? '#icon-opend-eye'
-                            : '#icon-closed-eye')
-                        }
-                      ></use>
-                    </svg>
-                  </button>
-                  <input
-                    type={showPassword ? 'text' : 'password'}
-                    placeholder="Password"
-                    name="password"
-                  />
-                </PasswordInputContainer>
-              </div>
-            </PasswordBox>
-            <BtnSettingSave type="submit">Save</BtnSettingSave>
-          </FormStyle>
-        </Modal>
+                <PasswordBox>
+                  <h3>Password</h3>
+                  <div>
+                    <h4>Outdated password:</h4>
+                    <PasswordInputContainer>
+                      <button type="button" onClick={togglePasswordVisibility}>
+                        <svg width="16" height="16">
+                          <use
+                            href={icons + (showPassword ? '#icon-opend-eye' : '#icon-closed-eye')}
+                          ></use>
+                        </svg>
+                      </button>
+                      <input
+                        type={showPassword ? 'text' : 'password'}
+                        placeholder="Password"
+                        name="password"
+                      />
+                    </PasswordInputContainer>
+                  </div>
+                  <div>
+                    <h4>New Password:</h4>
+                    <PasswordInputContainer>
+                      <button type="button" onClick={togglePasswordVisibility}>
+                        <svg width="16" height="16">
+                          <use
+                            href={icons + (showPassword ? '#icon-opend-eye' : '#icon-closed-eye')}
+                          ></use>
+                        </svg>
+                      </button>
+                      <input
+                        type={showPassword ? 'text' : 'password'}
+                        placeholder="Password"
+                        name="password"
+                      />
+                    </PasswordInputContainer>
+                  </div>
+                  <div>
+                    <h4>Repeat new password:</h4>
+                    <PasswordInputContainer>
+                      <button type="button" onClick={togglePasswordVisibility}>
+                        <svg width="16" height="16">
+                          <use
+                            href={icons + (showPassword ? '#icon-opend-eye' : '#icon-closed-eye')}
+                          ></use>
+                        </svg>
+                      </button>
+                      <input
+                        type={showPassword ? 'text' : 'password'}
+                        placeholder="Password"
+                        name="password"
+                      />
+                    </PasswordInputContainer>
+                  </div>
+                </PasswordBox>
+              </FormWrapper>
+              <BtnSettingSave>
+                <button type="submit">Save</button>
+              </BtnSettingSave>
+            </FormStyle>
+          </Modal>
+        </WrapperModal>
       </Backdrop>
     </>
   );
 };
 
 export default Setting;
-
-// lplplp@lp.lp
