cf40722201ebbdeccab052007253fc07d5444976
barinio <110419878+barinio@users.noreply.github.com>
2024-02-01 13:18:25 +0000

    Deploying to gh-pages from @ barinio/project-drink-it@5c793b0ae6600a8d9455b45636c6587de8da8355 🚀

---
+++ static/js/793.795601da.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[793],{4793:function(e,t,c){c(2791);t.Z=c.p+"static/media/icons.80148817591fc248f9519d9960571e79.svg"}}]);
+//# sourceMappingURL=793.795601da.chunk.js.map
