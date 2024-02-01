11f031f817b664fc020625e36b8b42492811028a
KaterinichViktor <vspasedust@gmail.com>
2024-02-01 22:45:43 +0200

    fix

--- src/redux/dailyNorma/dailyNormaThunk.js
+++ src/redux/dailyNorma/dailyNormaThunk.js
@@ -2,25 +2,43 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { getDailyNormaData, updateDailyNormaData } from 'services/api';
 
-export const getDailyNorma = createAsyncThunk(
-  'auth/getDailyNorma',
-  async (_, thunkAPI) => {
-    try {
-      const data = await getDailyNormaData();
-      return data;
-    } catch (error) {
-      return thunkAPI.rejectWithValue(error.message);
-    }
-  }
-);
+// export const getDailyNorma = createAsyncThunk(
+//   'auth/getDailyNorma',
+//   async (_, thunkAPI) => {
+//     try {
+//       const data = await getDailyNormaData();
+//       return data;
+//     } catch (error) {
+//       return thunkAPI.rejectWithValue(error.message);
+//     }
+//   }
+// );
 
-export const updateDailyNorma = createAsyncThunk(
-  'auth/updateDailyNorma',
-  async (userData, thunkAPI) => {
-    try {
-      await updateDailyNormaData(userData);
-    } catch (error) {
-      return thunkAPI.rejectWithValue(error.message);
-    }
+// export const updateDailyNorma = createAsyncThunk(
+//   'auth/updateDailyNorma',
+//   async (userData, thunkAPI) => {
+//     try {
+//       await updateDailyNormaData(userData);
+//     } catch (error) {
+//       return thunkAPI.rejectWithValue(error.message);
+//     }
+//   }
+// );
+
+export const getDailyNorma = createAsyncThunk('auth/getDailyNorma', async ({ id, _id }, { rejectWithValue }) => {
+  try {
+    const response = await getDailyNormaData({ id, _id });
+    return response;
+  } catch (error) {
+    return rejectWithValue(error.message);
   }
-);
+});
+
+export const updateDailyNorma = createAsyncThunk('auth/updateDailyNorma', async ({ id, _id, userData }, { rejectWithValue }) => {
+  try {
+    const response = await updateDailyNormaData({ id, _id, userData });
+    return response;
+  } catch (error) {
+    return rejectWithValue(error.message);
+  }
+});
