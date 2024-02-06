2e0b9cb29d30eeeb37c36380324fc0643afc1a13
rosyven <93422035+rosyven@users.noreply.github.com>
2024-02-06 15:54:00 +0200

    welcomepage, signin, signup en, uk, fr done

--- src/i18n/copies/fr.js
+++ src/i18n/copies/fr.js
@@ -1,3 +1,38 @@
 export const fr = {
   test: 'Français',
+  signup: 'Inscription',
+  signin: 'Connexion',
+  // welcome page
+  textLeft: {
+    title: "Suivi de la consommation d'eau",
+    text1: "Enregistrez l'apport quotidien en eau et suivez",
+    text2: 'Avantages du suivi',
+    text3: 'Habitude de conduite',
+    text4: 'Voir les statistiques',
+    text5: 'Réglage du taux personnel',
+  },
+  textRight: {
+    listTitle: "Pourquoi boire de l'eau",
+    listItem1: 'Fourniture de nutriments à tous les organes',
+    listItem2: "Fourniture d'oxygène aux poumons",
+    listItem3: 'Maintien du fonctionnement du cœur',
+    listItem4: 'Élimination des substances traitées',
+    listItem5: "Assurer la stabilité de l'environnement interne",
+    listItem6: "Maintien d'une température corporelle normale'",
+    listItem7: "Maintien d'un système immunitaire capable de résister aux maladies",
+  },
+
+  forms: {
+    email: 'Entrez votre adresse e-mail',
+    password: 'Entrez votre mot de passe',
+    repeatPassword: 'Répétez le mot de passe',
+  },
+
+  buttons: {
+    welcomepage: 'Essayez le tracker',
+    signup: "S'inscrire",
+    signin: 'Se connecter',
+    logout: 'Se déconnecter',
+    settings: 'Paramètres',
+  },
 };
