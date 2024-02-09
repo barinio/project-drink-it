5079e7fb789a8dac34adbd394fb0e9ba33d5ad13
Ваня <7328652@gmail.com>
2024-02-09 03:08:11 +0200

    changed msg in console about server awake

--- src/services/WakeUpServer.jsx
+++ src/services/WakeUpServer.jsx
@@ -5,7 +5,7 @@ const WakeUpServer = () => {
   useEffect(() => {
     const wakeUp = async () => {
       try {
-        console.log('Server activation');
+        console.log("The server was nudged, and it's stirring awake🥱");
         await axios.get('https://project-drink-it-backend.onrender.com');
       } catch (error) {
         console.log('The server just woke up✅');
